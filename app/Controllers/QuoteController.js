820484c2223687030204663e927efc4e06afb1b9
Steffen Larson <steffman13@gmail.com>
2021-01-10 16:26:50 -0700

    Working on quote

--- app/Controllers/QuoteController.js
+++ app/Controllers/QuoteController.js
@@ -1,3 +1,10 @@
 
 //TODO Create methods for constructor, and rendering the quote to the page
-export default class QuoteController { }
+
+
+
+export default class QuoteController {
+  constructor() {
+
+  }
+}
--- app/models/Quote.js
+++ app/models/Quote.js
@@ -1,3 +1,20 @@
 export default class Quote {
+  constructor(data) {
+    this.quoteId = data.quote.id
+    this.author = data.author
+    this.body = data.body
+  }
 
+  get Template() {
+    return `
+                <div class="card">
+                  <div class="card-body">
+
+                    <h5 class="hoverable text-center">${this.body}</h5>
+                    <p class="hide text-center">${this.author}</p>
+
+                  </div>
+                </div>
+    `
+  }
 }
